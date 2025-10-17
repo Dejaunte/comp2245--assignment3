c4f55ec47dfc46689bdaaa505ab7481e6911e92e
Dejaunte Adams <500001483@my.fiveislands.uwi.edu>
2025-10-17 10:47:12 -0400

    Completed and updated With Exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,7 @@
 // tic-tac-toe
 
 // Create an event listner 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function() {
     //board element
     const board = document.getElementById("board");
 
@@ -11,5 +11,28 @@ document.addEventListener('DOMContentLoaded', function() {
     // add squares
     squares.forEach(square => {
         square.classList.add("square");
+    });
+
+
+//create current player variable
+    let currentPlayer = "X";
+
+// set game state
+    let gameActiveState = ["", "", "", "", "", "", "", "", ""];
+
+// Add click event listener to each square
+    squares.forEach((square, index) => {
+        square.addEventListener("click", function() {
+            // click if square is empty
+                if (gameActiveState[index] === "") {
+            // update game state
+                gameActiveState[index] = currentPlayer;
+            // display current player symbol
+                square.innerText = currentPlayer;
+                square.classList.add(currentPlayer);
+            // switch player
+                currentPlayer = currentPlayer === "X" ? "O" : "X";
+            }
+        });
     });
 });
